409f211e19a3bdc77ce66a708005e78cf2096933
Cellie <CelliesProjects@users.noreply.github.com>
2025-04-09 11:04:04 +0200

    Header file from `.hpp` to `.h`

--- src/ScreenShot.cpp
+++ src/ScreenShot.cpp
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include "ScreenShot.hpp"
+#include "ScreenShot.h"
 
 bool ScreenShot::writeHeader(const lgfx::LGFXBase &gfx, File &file)
 {
